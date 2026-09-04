c74561e19d21ce751e306fe2cc9eee03033ae6c7
kao <vera.yu.382@gmail.com>
2026-09-04 20:08:58 +0000

    fixed errors again

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 /* See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment for details. */
 function publicContext(ctx) {
-  const { ...rest } = ctx;
+  const rest = {...ctx};
   return rest;
 }
 
